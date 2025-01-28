1523a2e0a76aef901af06110c2f5523b297d2b50
Max Harris <max@harrisfamilydesigns.com>
2025-01-27 22:25:19 -0500

    placeholder categories and iamges

--- api/glutenFreeProducts.ts
+++ api/glutenFreeProducts.ts
@@ -3,12 +3,33 @@ import { GlutenFreeProduct } from "@/types"
 
 export const fetchGlutenFreeProducts = async ({
   limit = 10,
-  offset = 0
+  offset = 0,
+  search = ''
 }: {
   limit: number
   offset: number
+  search?: string
 }): Promise<GlutenFreeProduct[]> => {
-  const { data, error } = await supabase.from('gf_products').select('*').range(offset, offset + limit - 1);
+  const searchTerms = search.split(' ').filter(Boolean);
+
+  let query = supabase
+    .from("gf_products")
+    .select("*")
+    .range(offset, offset + limit - 1);
+
+  // Dynamically apply the OR filter if search terms exist
+  if (searchTerms.length > 0) {
+    const orFilters = searchTerms
+      .map(
+        (term) =>
+          `name.ilike.%${term}%,brand_name.ilike.%${term}%` // OR conditions for each term
+      )
+      .join(",");
+
+    query = query.or(orFilters); // Apply the OR filter
+  }
+
+  const { data, error } = await query;
   if (error) throw new Error(error.message);
 
   return data;
--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,12 +1,29 @@
 import { Box } from '@/components/ui/box';
 import { Text } from '@/components/ui/text';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
-import { fetchGlutenFreeProducts, fetchSession, queryKeys } from '@/api';
-import UserAvatar from '@/components/UserAvatar/UserAvatar';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { fetchGlutenFreeProducts, queryKeys } from '@/api';
 import { ActivityIndicator, FlatList } from 'react-native';
 import SearchInput from '@/components/SearchInput/SearchInput';
-import { Fragment, useState } from 'react';
+import { useEffect, useState } from 'react';
+import { Button, ButtonIcon, ButtonText } from '@/components/ui/button';
+import { ChevronRightIcon } from '@/components/ui/icon';
+import { Image } from '@/components/ui/image';
+
+const image_url = "https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+const categories = [
+  { id: 1, name: "Baked goods", image_url },
+  { id: 2, name: "Bread", image_url },
+  { id: 3, name: "Meat", image_url },
+  { id: 4, name: "Dairy", image_url },
+  { id: 5, name: "Fruits", image_url },
+  { id: 6, name: "Vegetables", image_url },
+  { id: 7, name: "Cereals", image_url },
+  { id: 8, name: "Pasta", image_url },
+  { id: 9, name: "Sauces", image_url },
+  { id: 10, name: "Snacks", image_url },
+  { id: 11, name: "Drinks", image_url },
+];
 
 export default function Home() {
   const [searchValue, setSearchValue] = useState('');
@@ -19,18 +36,30 @@ export default function Home() {
     fetchNextPage,
     hasNextPage,
     isFetchingNextPage,
+    refetch,
   } = useInfiniteQuery({
     queryKey: [queryKeys.GLUTEN_FREE_PRODUCTS],
-    queryFn: ({ pageParam = 0 }) => fetchGlutenFreeProducts({ limit: perPage, offset: pageParam }),
+    queryFn: ({ pageParam = 0 }) => fetchGlutenFreeProducts({
+      limit: perPage,
+      offset: pageParam,
+      search: searchValue
+    }),
     initialPageParam: 0,
     getNextPageParam: (lastPage, allPages) => {
       // Determine if there are more pages to fetch
       return lastPage.length < perPage ? null : allPages.length * perPage;
     },
   });
-  const isLoading = productsIsLoading;
 
-  if (isLoading) {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      refetch();
+    }, 500);
+
+    return () => clearTimeout(timeout);
+  }, [searchValue]);
+
+  if (productsIsLoading) {
     return <Text>Loading...</Text>;
   }
 
@@ -43,11 +72,40 @@ export default function Home() {
 
   return (
     <SafeAreaView edges={['bottom']} className="flex flex-col flex-1">
-      <Box className="mt-3 mx-3">
+      {/* Search for glutenFreeProducts */}
+      <Box className="mt-5 mx-3">
         <SearchInput value={searchValue} onChangeText={setSearchValue} />
       </Box>
 
-      <Box className="flex-1">
+      {/* categories horizontal scrolling list */}
+      <Box className="mt-8 flex flex-row items-center justify-between mx-3">
+        <Text bold size='xl'>
+          Categories
+        </Text>
+        <Button variant='link' size="sm" className="ml-2">
+          <ButtonText className="bold text-gray-400">See all</ButtonText>
+          <ButtonIcon as={ChevronRightIcon} />
+        </Button>
+      </Box>
+
+      <Box>
+        <FlatList
+          horizontal
+          data={categories}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <Box key={item.id.toString()} className="p-4 h-40 w-40 bg-gray-200 rounded-lg ml-3">
+              <Text bold size='xl'>{item.name}</Text>
+              <Box>
+                <Image source={{ uri: item.image_url }} className="w-full h-24" />
+              </Box>
+            </Box>
+          )}
+        />
+      </Box>
+
+      {/* glutenFreeProducts list */}
+      <Box className="flex-1 mt-3">
         <FlatList
           scrollsToTop
           data={glutenFreeProducts}
